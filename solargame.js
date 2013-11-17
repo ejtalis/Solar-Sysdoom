fcdd0ebe1df127c276499731256c6f3c696c3e50
jschmidt777 <schmidtjoseph39@gmail.com>
2013-11-17 15:59:32 -0500

    Added Functions
    I added a function for if the user gets the question wrong or right, and
    I tried to develop Mercury function but I'm not sure if it is right.

--- solargame.js
+++ solargame.js
@@ -7,6 +7,8 @@ planet=document.getElementById('actionInput').value;
  if   (planet.toLowerCase()==='mercury') {
   
   displayPlanet( 'To Mercury!'); 
+	
+  Mercury();
     
 	}   
 	 else if (planet.toLowerCase()==='venus') {
@@ -56,7 +58,15 @@ planet=document.getElementById('actionInput').value;
        
       
            
-        
+function Correct()
+{
+alert('Correct! Type in another name of a Planet to get the next piece of the device!');
+}      
+
+function Wrong()
+{
+alert('Try Again!');
+}  
 
 
   
@@ -70,12 +80,39 @@ document.getElementById('outputDiv').innerHTML=message;
 }
 
 
-function Mercury(answer)
+function Mercury()
 
 {
-	return RandomOneOf([]);
+	var mercQuestions= [1. How many moons does Mercury have? Enter a number (i.e. '4', not 'four').,
+2. How many Earth days does it take for Mercury to rotate once around its axis? Enter a number (i.e. '4', not 'four') and round your answer to the nearest whole number.,
+3. What is the polar diameter of Mercury, in km? Enter a number (i.e. '4', not 'four') and do not include commas (i.e. '3997', not '3,997').,
+4. Mercury be observed, from Earth, passing across the face of the Sun. This event is called a transit. How many times per century does this transit of Mercury occur? Enter a number (i.e. '4', not 'four').,]
+  
+	var mercAnswers= [0,88,4879,17]
 	
-	if (answer.charAt(0)===)
+	return RandomOneOf(mercQuestions);
+	
+	if (mercQuestions.indexOf(0)===mercAnswers.indexOf(0)) {
+	
+	Correct();
+	}
+	else if (mercQuestions.indexOf(1)===mercAnswers.indexOf(1)) {
+	
+	Correct();
+	}
+	else if (mercQuestions.indexOf(2)===mercAnswers.indexOf(2)) {
+	
+	Correct();
+	}
+	else if (mercQuestions.indexOf(3)===mercAnswers.indexOf(3)) {
+	
+	Correct();
+	}
+	
+	else {
+	Wrong();
+	}
+
 }
 
     //Hello Joe
