0d8a3b37ba146fe2fb7fede018b354bca98023b5
jschmidt777 <jschmidt777@users.noreply.github.com>
2013-11-19 18:54:31 -0500

    Update solargame.js

--- solargame.js
+++ solargame.js
@@ -81,10 +81,10 @@ document.getElementById('outputDiv').innerHTML=message;
 function mercuryDoom()
 
 {
-	var mercQuestions= ['1. How many moons does Mercury have? Enter a number (i.e. \'4\', not \'four\').',
-'2. How many Earth days does it take for Mercury to rotate once around its axis? Enter a number (i.e. \'4\', not \'four\') and round your answer to the nearest whole number.',
-'3. What is the polar diameter of Mercury, in km? Enter a number (i.e. \'4\', not \'four\') and do not include commas (i.e. \'3997\', not \'3,997\').',
-'4. Mercury be observed, from Earth, passing across the face of the Sun. This event is called a transit. How many times per century does this transit of Mercury occur? Enter a number (i.e. \'4\', not \'four\').',]
+	var mercQuestions= ['1. How many moons does Mercury have? Enter a number (i.e. \"4\" not \"four\").',
+'2. How many Earth days does it take for Mercury to rotate once around its axis? Enter a number (i.e. \"4\", not \"four\") and round your answer to the nearest whole number.',
+'3. What is the polar diameter of Mercury, in km? Enter a number (i.e. \"4\", not \"four\") and do not include commas (i.e. \"3997\", not \"3,997\").',
+'4. Mercury be observed, from Earth, passing across the face of the Sun. This event is called a transit. How many times per century does this transit of Mercury occur? Enter a number (i.e. \"4\", not \"four\").',]
   
 	var mercAnswers= ['0','88','4879','17']
 	
