f6667af57d16f8a83c1dbdbc949594730a6f9305
ejtalis <emma.talis1@marist.edu>
2013-11-20 12:07:38 -0500

    Update solargame.js
    Fixing some stuff like quotes and adding lots of semi-colins!

--- solargame.js
+++ solargame.js
@@ -52,12 +52,12 @@ planet=document.getElementById('actionInput').value;
  }
         
     
-function Correct()
+function correct()
 {
 alert('Correct! Type in another name of a Planet to get the next piece of the device!');
 }      
 
-function Wrong()
+function wrong()
 {
 alert('Try Again!');
 }   
@@ -73,28 +73,28 @@ document.getElementById('outputDiv').innerHTML=message;
 function mercuryDoom()
 {
 	var mercQuestions=new Array();
-	mercQuestions[0]=“How many moons does Mercury have? Enter a number (i.e. ’4’ not ’four’).”;
-	mercQuestions[1]=“How many Earth days does it take for Mercury to rotate once around its axis? Enter a number (i.e. ‘4’, not ‘four’) and round your answer to the nearest whole number.”;
-	mercQuestions[2]=“What is the polar diameter of Mercury, in km? Enter a number (i.e. ‘4’, not ‘four’) and do not include commas (i.e. ‘3997’, not ‘3,997’).”;
-	mercQuestions[3]=“Mercury be observed, from Earth, passing across the face of the Sun. This event is called a transit. How many times per century does this transit of Mercury occur? Enter a number (i.e. ‘4’, not ‘four’).”;
+	mercQuestions[0]="How many moons does Mercury have? Enter a number (i.e. ’4’ not ’four’).";
+	mercQuestions[1]="How many Earth days does it take for Mercury to rotate once around its axis? Enter a number (i.e. ‘4’, not ‘four’) and round your answer to the nearest whole number.";
+	mercQuestions[2]="What is the polar diameter of Mercury, in km? Enter a number (i.e. ‘4’, not ‘four’) and do not include commas (i.e. ‘3997’, not ‘3,997’).";
+	mercQuestions[3]="Mercury be observed, from Earth, passing across the face of the Sun. This event is called a transit. How many times per century does this transit of Mercury occur? Enter a number (i.e. ‘4’, not ‘four’).";
   
-	var mercAnswers= [“0”,”88”,”4879”,”17”]
+	var mercAnswers=["0","88","4879","17"];
 	
-	var pos= RandomInt(0,mercQuestions.length)
+	var pos=RandomInt(0,mercQuestions.length);
 	
-	var ques= mercQuestions[pos]
+	var ques=mercQuestions[pos];
 	
-	var ans= mercAnswers[pos]
+	var ans=mercAnswers[pos];
 	
-	document.getElementById('outputDiv').innerHTML= ques;
+	document.getElementById('outputDiv').innerHTML=ques;
 	
-	userAns= document.getElementById('answerInput').value
+	userAns=document.getElementById('answerInput').value;
 	
 	if (userAns===ans) { 
-		 Correct();
+		 correct();
 	}
 	else {
-		Wrong();
+		wrong();
 	}
 
     //Hello Joe
