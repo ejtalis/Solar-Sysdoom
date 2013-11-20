01e905fe6f58a33563b7962784d38d1b3f654b5f
ejtalis <emma.talis1@marist.edu>
2013-11-20 12:43:52 -0500

    Changed around mercuryDoom() and added mercuryCheckAnswer, after changing onclick for 2nd button in html file. mercuryDoom() is to be called right away (by the user entering mercury) and so the question will be choosen and displayed to the user; mercuryCheckAnswer is only called after the user enters their answer and presses the button.

--- solargame.js
+++ solargame.js
@@ -6,6 +6,7 @@ planet=document.getElementById('actionInput').value;
  if   (planet.toLowerCase()==='mercury') {
   
   displayPlanet( 'To Mercury!'); 
+  mercuryDoom();
     
 	}   
 	 else if (planet.toLowerCase()==='venus') {
@@ -48,7 +49,6 @@ planet=document.getElementById('actionInput').value;
 	   displayPlanet('You cannot travel there!');
            
 	 }
-         
  }
         
     
@@ -62,7 +62,6 @@ function wrong()
 alert('Try Again!');
 }   
    
-
   
 function displayPlanet(message)
 {
@@ -72,29 +71,38 @@ document.getElementById('outputDiv').innerHTML=message;
 
 function mercuryDoom()
 {
-	var mercQuestions=new Array();
+	mercQuestions=new Array();
 	mercQuestions[0]="How many moons does Mercury have? Enter a number (i.e. ’4’ not ’four’).";
 	mercQuestions[1]="How many Earth days does it take for Mercury to rotate once around its axis? Enter a number (i.e. ‘4’, not ‘four’) and round your answer to the nearest whole number.";
 	mercQuestions[2]="What is the polar diameter of Mercury, in km? Enter a number (i.e. ‘4’, not ‘four’) and do not include commas (i.e. ‘3997’, not ‘3,997’).";
 	mercQuestions[3]="Mercury be observed, from Earth, passing across the face of the Sun. This event is called a transit. How many times per century does this transit of Mercury occur? Enter a number (i.e. ‘4’, not ‘four’).";
   
-	var mercAnswers=["0","88","4879","17"];
+	mercAnswers=["0","88","4879","17"];
 	
-	var pos=RandomInt(0,mercQuestions.length);
+	mercPos=RandomInt(0,mercQuestions.length);
 	
-	var ques=mercQuestions[pos];
+	mercRandomQues=mercQuestions[pos];
 	
-	var ans=mercAnswers[pos];
+	mercRandomQAns=mercAnswers[pos];
 	
-	document.getElementById('outputDiv').innerHTML=ques;
+	document.getElementById('outputDiv').innerHTML=mercRandomQues;
 	
+}
+
+
+function mercuryCheckAnswer()
+{
 	userAns=document.getElementById('answerInput').value;
 	
-	if (userAns===ans) { 
+	if (mercRandomQAns===userAns) { 
 		 correct();
+	}
+	else if (userAns===‘’) {
+		alert(‘Please type in our answer!’);
 	}
 	else {
 		wrong();
 	}
+}
 
     //Hello Joe
