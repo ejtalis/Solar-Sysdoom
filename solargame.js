0cde4f25ffc30d76f55e8e5945d0e73f2701751f
ejtalis <emma.talis1@marist.edu>
2013-11-20 11:59:36 -0500

    Trying to fix arrays and mercuryDoom function

--- solargame.js
+++ solargame.js
@@ -1,5 +1,4 @@
 function travelToPlanet(planet)
-
 {
 
 planet=document.getElementById('actionInput').value;
@@ -54,39 +53,32 @@ planet=document.getElementById('actionInput').value;
         
     
 function Correct()
-
 {
 alert('Correct! Type in another name of a Planet to get the next piece of the device!');
 }      
 
 function Wrong()
-
 {
 alert('Try Again!');
 }   
-           
-        
-
+   
 
   
 function displayPlanet(message)
-
 {
-
-
 document.getElementById('outputDiv').innerHTML=message;  
-
 }
 
-function mercuryDoom()
 
+function mercuryDoom()
 {
-	var mercQuestions= ['1. How many moons does Mercury have? Enter a number (i.e. \"4\" not \"four\").',
-'2. How many Earth days does it take for Mercury to rotate once around its axis? Enter a number (i.e. \"4\", not \"four\") and round your answer to the nearest whole number.',
-'3. What is the polar diameter of Mercury, in km? Enter a number (i.e. \"4\", not \"four\") and do not include commas (i.e. \"3997\", not \"3,997\").',
-'4. Mercury be observed, from Earth, passing across the face of the Sun. This event is called a transit. How many times per century does this transit of Mercury occur? Enter a number (i.e. \"4\", not \"four\").',]
+	var mercQuestions=new Array();
+	mercQuestions[0]=“How many moons does Mercury have? Enter a number (i.e. ’4’ not ’four’).”;
+	mercQuestions[1]=“How many Earth days does it take for Mercury to rotate once around its axis? Enter a number (i.e. ‘4’, not ‘four’) and round your answer to the nearest whole number.”;
+	mercQuestions[2]=“What is the polar diameter of Mercury, in km? Enter a number (i.e. ‘4’, not ‘four’) and do not include commas (i.e. ‘3997’, not ‘3,997’).”;
+	mercQuestions[3]=“Mercury be observed, from Earth, passing across the face of the Sun. This event is called a transit. How many times per century does this transit of Mercury occur? Enter a number (i.e. ‘4’, not ‘four’).”;
   
-	var mercAnswers= ['0','88','4879','17']
+	var mercAnswers= [“0”,”88”,”4879”,”17”]
 	
 	var pos= RandomInt(0,mercQuestions.length)
 	
@@ -98,8 +90,7 @@ function mercuryDoom()
 	
 	userAns= document.getElementById('answerInput').value
 	
-	if (userAns===ans) {
-		 
+	if (userAns===ans) { 
 		 Correct();
 	}
 	else {
